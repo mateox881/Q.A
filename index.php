71d4f69596c6be68e7e9e570fdfab00083ca703b
el MATAPERRAS <amvega19@misena.edu.co>
2021-02-17 21:04:18 -0500

    cambio gay 2
    hdh

--- index.php
+++ index.php
@@ -1,42 +1,168 @@
 <!DOCTYPE html>
-<html lang="es">
+<html>
 <head>
-	<meta charset="UTF-8">
-    <title>Ingreso</title> 
-    <link rel="icon" type="img/png" href="imagenes/Logo.png">
-	<meta name="viewport" content="width=device-width, user-scalable=yes, initial-scale=1.0, maximum-scale=3.0, minimum-scale=1.0">
- <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" >
- <link rel="stylesheet" type="text/css" href="estilos.css" />
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-    <link href="https://fonts.googleapis.com/css2?family=Staatliches&display=swap" rel="stylesheet">
-</head>  
+<link rel="stylesheet" type="text/css" href="estilos.css" />
+<link rel="icon" type="img/png" href="imagenes/Logo.png">
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<!-- Font Awesome -->
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css">
+<!-- Google Fonts -->
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap">
+<!-- Bootstrap core CSS -->
+<link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.0/css/bootstrap.min.css" rel="stylesheet">
+<!-- Material Design Bootstrap -->
+<link href="https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.19.1/css/mdb.min.css" rel="stylesheet">
+
+<title>Pagina principal</title>
+
+</head>
 <body>
-    <img id="logo" src="imagenes/Logo.png" style="width: 100px; float:left; margin-top:-80px;">   
-    <h1 id="titulo1">Welcome to Kronnos</h1>
-    
-    <form class="formulario" id="form1" name="form1" method="POST" action="evaluar_sesion.php">
-        
-        <h1 id="login1">INGRESO</h1>
-        <div class="contenedor">
+<select class="body view">
+    <div class="mask rgba-black-ligh" ></div>
+</select>
 
-                <div class="input-contenedor">
-                    <div id="cc">
-                        <i class="glyphicon glyphicon-credit-card"style="color:gray"></i>
-                        &nbsp;&nbsp;&nbsp;
-                        <input type="text" placeholder="Cedula" name="documento" required="">
-                    </div>
-                </div>
+    <div id="menu">
+        <?php
+        include ("menu.php");
+        ?>
+    </div>
+   
+    <div id="general"> 
 
-                <div class="input-contenedor">
-                    <i class="fas fa-key icon"></i>
-                    <input type="password" placeholder="Contraseña" name="contrasena" required="">
-                </div>
-                
-          <input type="submit" value="Inicio" class="button">
-            <p>¿No tienes una cuenta? <a class="link" href="registrarvista.php">Registrate </a></p>
+
+    <div id="col1">
+    <section class="fondo1 view">
+    | 
+<div class="mask rgba-black-strong">
+
+
+
+    <div class="colum1">
+     <img src="https://i0.wp.com/www.ideediseno.co/wp-content/uploads/2020/04/idee-produccion.png?fit=184%2C250&ssl=1" alt="">
+    </div>
+    <div class="colum2">
+      <h1>Kronnos Inventory</h1>
+      <h3 id="text2">Manten tu inventario ordenado.</h3>
+      <span id="text2">Manten un registro exactamento el stock de la empresa  en cualquier momento, con una simple consulta.</span>
+            </div>
         </div>
-        
+    </div>
+    </section>
+
+
+
+    
+    </div>
+    </div> 
+    <div id="cuerpo" >
+
  
-        </form>
+
+    <div class="galeria">
+        <h1 id="galeria">Galeria </h1>
+        <div class="linea"></div>
+        <div class="contenedor-imagenes">
+            <div class="imagen">
+                <img src="imagenes/img1.jpg" alt="">
+                <div class="overlay">
+                <h2>COL-MASCOTAS</h2>
+                </div>
+            </div>
+            <div class="imagen">
+                <img src="imagenes/img2.jpg" alt="">
+                <div class="overlay">
+                <h2>COL-MASCOTAS</h2>
+                </div>
+            </div>
+            <div class="imagen">
+                <img src="imagenes/img3.jpg" alt="">
+                <div class="overlay">
+                <h2>COL-MASCOTAS</h2>
+                </div>
+            </div>
+            <div class="imagen">
+                <img src="imagenes/img4.jpg" alt="">
+                <div class="overlay">
+                <h2>COL-MASCOTAS</h2>
+                </div>
+            </div>
+            <div class="imagen">
+                <img src="imagenes/img5.jpg" alt="">
+                <div class="overlay">
+                <h2>COL-MASCOTAS</h2>
+                </div>
+            </div>
+            <div class="imagen">
+                <img src="imagenes/img6.jpg" alt="">
+                <div class="overlay">
+                <h2>COL-MASCOTAS</h2>
+                </div>
+            </div>
+            <div class="imagen">
+                <img src="imagenes/img7.jpg" alt="">
+                <div class="overlay">
+                <h2>COL-MASCOTAS</h2>
+                </div>
+            </div>
+            <div class="imagen">
+                <img src="imagenes/img8.jpg" alt="">
+                <div class="overlay">
+                <h2>COL-MASCOTAS</h2>
+                </div>
+            </div>
+            <div class="imagen">
+                <img src="imagenes/img9.jpg" alt="">
+                <div class="overlay">
+                    <h2>COL-MASCOTAS</h2>
+                </div>
+            </div>
+            <!-- <div class="imagen">
+                <img src="img/1 (2).jpg" alt="">
+            </div> -->
+        </div>
+    </div>
+
+
+
+
+    </div>
+    <div class="cuerpo2" >
+
+     <div id="hc">
+     <h3 id="galeria">¿Que deseas hacer?</h3>   
+     <a href="pre_crear_categoria.php"><button type="button" class="btn btn-outline-primary waves-effect">Crear categoria</button></a>
+     <a href="pre_crear_producto.php"><button type="button" class="btn btn-outline-default waves-effect" >Crear Producto</button></a>
+     <a href="pre_crear_producto.php"><button type="button" class="btn btn-outline-secondary waves-effect">Provedor</button></a> 
+     <a href="pre_editar_producto.php"><button type="button" class="btn btn-outline-success waves-effect">Modificar</button></a>
+     <a href=""><button type="button" class="btn btn-outline-info waves-effect">preguntas</button></a>
+     <a href="pre_consultar_productos.php"><button type="button" class="btn btn-outline-warning waves-effect">consultar</button></a>
+     <a href=""><button type="button" class="btn btn-outline-danger waves-effect">Danger</button></a>
+     </div>
+    
+    </div>
+
+    <div id="col2">
+        <?php
+        include ("col2.php");
+        ?>
+</div> 
+    
+    <div id="footer">
+    <?php
+        include ("footer.php");
+        ?>
+    </div>
+
+<!-- JQuery -->
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<!-- Bootstrap tooltips -->
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.4/umd/popper.min.js"></script>
+<!-- Bootstrap core JavaScript -->
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.0/js/bootstrap.min.js"></script>
+<!-- MDB core JavaScript -->
+<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.19.1/js/mdb.min.js"></script>
+
 </body>
+
 </html>
